3d1db81c090552f20a6c953787e7ac15561992cc
wissam khandji <khandji.w@gmail.com>
2021-03-09 08:42:03 +0100

    2

---
+++ script.js
@@ -0,0 +1,37 @@
+//Ville initiale
+let villeChoisie = "Dubai"
+recevoirTemperature(villeChoisie)
+
+//Click boutton changer de ville
+let changerVille = document.querySelector('#changer');
+changerVille.addEventListener('click', () => {
+    villeChoisie = prompt('Quelle ville voulez vous choisir ?');
+    recevoirTemperature(villeChoisie);
+})
+
+//requete AJAX ds une fonction
+function recevoirTemperature(){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${villeChoisie}&appid=d99897bcbb91f8c4224052c9f6e9db33&units=metric`
+    
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var reponse     = request.response;
+                let temperature = reponse.main.temp;
+                let ville       = reponse.name;
+
+                document.querySelector('#temperature_label').textContent = temperature;
+                document.querySelector('#ville').textContent = ville;
+            }
+            else{
+                alert('un probleme est survenu, revenez plus tard.')
+            }
+        }
+    }
+}
+
